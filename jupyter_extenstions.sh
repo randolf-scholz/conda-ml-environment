e11f81d478a1d5aa81debff8d65ab87c84898722
Randolf Scholz <randolf.scholz@gmail.com>
2021-10-26 16:24:30 +0200

    changed jupyterlab_templates to original repo after fix was merged

--- jupyter_extenstions.sh
+++ jupyter_extenstions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python -m pip install git+https://github.com/randolf-scholz/jupyterlab_templates.git
+python -m pip install git+https://github.com/jpmorganchase/jupyterlab_templates.git
 jupyter labextension install jupyterlab_templates
 jupyter serverextension enable --py jupyterlab_templates
 
